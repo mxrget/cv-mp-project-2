1440cb9452d5d7d19649e8e56f8d3f3b39146336
Mark <changforeorg@gmail.com>
2021-12-12 23:52:51 +0300

    отличаю ножницы от бумаги + журнал работы

--- smth.py
+++ smth.py
@@ -9,10 +9,17 @@ def get_points(landmark, shape):
         points.append([mark.x * shape[1], mark.y * shape[0]])
     return np.array(points, dtype=np.int32)
 
-def palm_size(landmark, shape):
+#def palm_size(landmark, shape): - кулак распознавать
     x1, y1 = landmark[0].x * shape[1], landmark[0].y * shape[0]
     x2, y2 = landmark[5].x * shape[1], landmark[5].y * shape[0]
     return ((x1 - x2)**2 + (y1 - y2) **2) **.5
+
+def palm_size(landmark, shape): # распознавание ножниц
+    x1, y1 = landmark[12].x * shape[1], landmark[12].y * shape[0]
+    x2, y2 = landmark[16].x * shape[1], landmark[16].y * shape[0]
+    return ((x1 - x2)**2 + (y1 - y2) **2) **.5
+
+#этот параметр для ножниц всегда меньше двух
 
 #создаем детектор
 handsDetector = mp.solutions.hands.Hands()
@@ -33,11 +40,11 @@ while cap.isOpened():
         (x, y), r = cv2.minEnclosingCircle(get_points(results.multi_hand_landmarks[0].landmark, flippedRGB.shape))
         ws = palm_size(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)
         print(2 * r / ws)
-        if 2 * r / ws > 1.3:
+        if 2 * r / ws > 1.35:
             cv2.circle(flippedRGB,(int(x), int(y)), int(r), (0, 0, 255), 2)
             # кулак разжат
             prev_fist = False
-        else:
+        elif 2 * r / ws < 1.8:
             cv2.circle(flippedRGB,(int(x), int(y)), int(r), (0, 255, 0), 2)
             if not prev_fist:
                 # произошло сжимание
