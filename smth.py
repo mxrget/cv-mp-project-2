f40e10a35c235a9b2096aae07c160dd6d114c1fd
Mark <changforeorg@gmail.com>
2021-12-21 23:40:35 +0300

    финалочка - теперь без отображения камеры, общение через консоль

--- smth.py
+++ smth.py
@@ -1,6 +1,8 @@
 from cv2 import cv2
 import mediapipe as mp
 import numpy as np
+import time
+import random
 
 
 def get_points(landmark, shape):
@@ -24,12 +26,8 @@ def palm_size_3(landmark, shape):
     x2, y2 = landmark[20].x * shape[1], landmark[20].y * shape[0]
     return ((x1 - x2) ** 2 + (y1 - y2) ** 2) **.5
 
-handsDetector = mp.solutions.hands.Hands()
-cap = cv2.VideoCapture(0)
-while cap.isOpened():
+def moment_detecting():
     ret, frame = cap.read()
-    if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
-        break
     flipped = np.fliplr(frame)
     flippedRGB = cv2.cvtColor(flipped, cv2.COLOR_BGR2RGB)
     results = handsDetector.process(flippedRGB)
@@ -38,19 +36,74 @@ while cap.isOpened():
         ws = palm_size(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)
         ws_2 = palm_size_2(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)
         ws_3 = palm_size_3(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)
-        print('1: ' + str(2 * r / ws))
-        print('2: ' + str(2 * r / ws_2))
-        print('3: ' + str(2 * r / ws_3))
-        print('-------------------------')
-        if 2 * r / ws < 1.65 and 2 * r / ws_2 > 4 and 2 * r / ws_3 > 1.35:
-            cv2.putText(flippedRGB, 'rock', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 2.5, (255, 0, 0), thickness = 3)
-        elif 2 * r / ws < 2.5 and 2 * r / ws_2 < 3 and 2 * r / ws_3 > 2.9:
-            cv2.putText(flippedRGB, 'scissors', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 2.5, (0, 255, 0), thickness = 3)
-        elif 2 * r / ws > 1.8 and 2 * r / ws_2 > 3 and 2 * r / ws_3 < 1.8:
-            cv2.putText(flippedRGB, 'paper', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 2.5, (0, 0, 255), thickness = 3)
+        if 2 * r / ws < 1.8 and 2 * r / ws_2 > 4 and 2 * r / ws_3 > 1.2:
+            return 'rock'
+        elif 2 * r / ws < 2.6 and 2 * r / ws_2 < 3 and 2 * r / ws_3 > 2.3:
+            return 'scissors'
+        elif 2 * r / ws > 1.8 and 2 * r / ws_2 > 3 and 2 * r / ws_3 < 2.3:
+            return 'paper'
         else:
-            cv2.putText(flippedRGB, 'unrecognizable', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 2.5, (255, 255, 255), thickness = 3)
-    res_image = cv2.cvtColor(flippedRGB, cv2.COLOR_RGB2BGR)
-    cv2.imshow('play', res_image)
+            return 'unrecognized'
+
+handsDetector = mp.solutions.hands.Hands()
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret, frame = cap.read()
+    flipped = np.fliplr(frame)
+    flippedRGB = cv2.cvtColor(flipped, cv2.COLOR_BGR2RGB)
+    result = moment_detecting()
+    ret, frame = cap.read()
+    flipped = np.fliplr(frame)
+    flippedRGB = cv2.cvtColor(flipped, cv2.COLOR_BGR2RGB)
+    results = handsDetector.process(flippedRGB)
+    if results.multi_hand_landmarks is not None:
+        computer_choice = random.choice(['rock', 'paper', 'scissors'])
+        time.sleep(1)
+        print('Нашёл руку в кадре! Показывай жест через 3....')
+        time.sleep(1)
+        print('2....')
+        time.sleep(1)
+        print('1....')
+        time.sleep(1)
+        result = moment_detecting()
+        if result == 'rock':
+            print('Кажется, ты выбрал камень...')
+            if computer_choice == 'rock':
+                print('Ничья! Компьютер тоже выбрал камень')
+                break
+            elif computer_choice == 'paper':
+                print('Ты проиграл :( компьютер выбрал бумагу!')
+                break
+            elif computer_choice == 'scissors':
+                print('Победа! Компьютер выбрал ножницы!')
+                break
+        elif result == 'paper':
+            print('Кажется, ты выбрал бумагу...')
+            if computer_choice == 'rock':
+                print('Победа! Компьютер выбрал камень')
+                break
+            elif computer_choice == 'paper':
+                print('Ничья! Выбор компьютера тоже пал на бумагу')
+                break
+            elif computer_choice == 'scissors':
+                print('Поражение - компьютер разрезал бумагу своими ножницами')
+                break
+        elif result == 'scissors':
+            print('Кажется, ты выбрал ножницы...')
+            if computer_choice == 'rock':
+                print('Компьютер победил - он выбрал камень')
+                break
+            elif computer_choice == 'paper':
+                print('Победа! Компьютер выбрал бумагу')
+                break
+            elif computer_choice == 'scissors':
+                print('Ничья! Ваши мнения с компьютером совпали - оба выбрали ножницы')
+                break
+        else:
+            print('Я не смог распознать, что именно ты хотел показать :/')
+            break
+    else:
+        print('не вижу руку.....')
+
 
 handsDetector.close()
